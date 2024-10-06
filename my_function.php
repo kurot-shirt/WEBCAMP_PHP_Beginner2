a3e241d175182232c8c256c27d674de211d6d594
aiko <aiko@example.com>
2024-10-06 10:17:55 +0000

    my_function.phpを書いた

---
+++ my_function.php
@@ -0,0 +1,19 @@
+<?php
+
+function numToDouble($i): int
+{
+    $ret = $i * 2;
+    
+    return $ret;
+}
+$num = numToDouble(10);
+echo "10を2倍したら{$num}になった \n";
+
+function numAdd($a, $b): int
+{
+    $ret = $a + $b;
+    
+    return $ret;
+}
+$add = numAdd(5, 6);
+echo "5と6を足したら{$add}になった \n";
